081339ff5b78382fd81b653a72a06d171e26ef7c
Rohith Teja M <rohithtejam@gmail.com>
2022-05-03 09:57:22 +0200

    Create loop.sh

---
+++ loop.sh
@@ -0,0 +1,8 @@
+for dim_red_type in 'pca' 'lle'; do
+    for n_comp in 5 10; do
+        for classifier in 'lr' 'svc' 'rf'; do
+                echo $dim_red_type $n_comp $classifier 
+                python argparse.py --dim_red_type "${dim_red_type}" --n_comp "${n_comp}" --classifier "${classifier}"
+        done
+    done
+done
